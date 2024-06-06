81f33cd04b1525a0dbd4b0d0e23569b503e2c009
KwartengA <andrewskwartengt@gmail.com>
2024-06-06 15:09:17 -0800

    Code for discount specification

--- calc.dart
+++ calc.dart
@@ -60,10 +60,17 @@ void main() {
 // Designating the number of times a user can place an order
  int chancesLeft = 3;
   while (chancesLeft > 0) {
+
         stdout.write('Enter product name (or type ''done'' to finish): ');
     String? productName = stdin.readLineSync();
     if (productName == 'done')
      break;
+
+     if (products.containsKey(productName)) {
+      stdout.write('Enter quantity: ');
+      int quantity = int.parse(stdin.readLineSync()!);
+
+     }
 
 
   }
