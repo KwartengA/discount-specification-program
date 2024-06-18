72698da6a607ec8846c6f7b6eae5f8751e1f132c
KwartengA <andrewskwartengt@gmail.com>
2024-06-17 23:51:27 -0800

    Code for discount specification

--- calc.dart
+++ calc.dart
@@ -120,7 +120,6 @@ void main() {
     double netTotal = totalPrice - customer.calculatediscount(totalprice);
 
     // Printing out the users's name and the total price of orders as well as their net-total price
-
     print('Hello, $name!');
      print('Your Total Price: \$${totalPrice.toStringAsFixed(2)}');
       print('Your Net Total: \$${netTotal.toStringAsFixed(2)}');
